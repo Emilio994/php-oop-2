acdf32614ae377ecb8a176a76b62f2f9d30d7e5c
Emilio94 <emilio.994@gmail.com>
2021-05-11 15:44:11 +0200

    Compito

---
+++ extension.php
@@ -0,0 +1,11 @@
+<?php
+
+    require 'user.php';
+
+    class HireDate extends User {
+        public function __construct($id, $name, $lastname, $date_of_birth, $task, $mail, $hire_date) {
+            parent::__construct($id, $name, $lastname, $date_of_birth, $task, $mail,$hire_date) ;
+            $this->hire_date = $hire_date;
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <?php
 
-require_once 'user.php';
+require_once 'extension.php';
 
-$giacomo = new User(1, 'Giacomo', 'Locatelli', '13/02/1992', 'impiegato', 'giacomo@email.it');
+$giacomo = new HireDate(1, 'Giacomo', 'Locatelli', '13/02/1992', 'impiegato', 'giacomo@email.it', '01-10-2015');
 $giacomo->setPermission();
 $giacomo->getUserData();
 
 
-$giovanna = new User(2, 'Giovanna', 'Rossi', '11/11/1987', 'impiegata', 'giovanna@email.it');
+$giovanna = new HireDate(2, 'Giovanna', 'Rossi', '11/11/1987', 'supervisore', 'giovanna@email.it', '12-03-2011');
 $giovanna->setPermission();
 $giovanna->getUserData();
 
-$marco = new User(3, 'Marco', 'Colombo', '13/08/1994', 'supervisore', 'marco@email.it');
+$marco = new HireDate(3, 'Marco', 'Colombo', '13/08/1994', 'impiegato', 'marco@email.it', '07-06-2017');
 $marco->setPermission();
 $marco->getUserData();
 
-$pietro = new User(4, 'Pietro', 'Zanardi', '01/01/1991', 'amministratore', 'pietro@email.it');
+$pietro = new HireDate(4, 'Pietro', 'Zanardi', '01/01/1991', 'amministratore', 'pietro@email.it', '10-10-2011');
 $pietro->setPermission();
 $pietro->getUserData();
 
-$rosa = new User(5, 'Rosa', 'Anghileri', '22/04/1976', 'segretaria', 'rosa@email.it');
+$rosa = new HireDate(5, 'Rosa', 'Anghileri', '22/04/1976', 'segretaria', 'rosa@email.it', '10-08-2014');
 $rosa->setPermission();
 $rosa->getUserData();
 
