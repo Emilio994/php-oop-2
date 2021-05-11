5237ce26da0bb9e3ce2a1474434acf47803b619a
Emilio94 <emilio.994@gmail.com>
2021-05-11 15:45:56 +0200

    Compito

--- extension.php
+++ extension.php
@@ -4,7 +4,7 @@
 
     class HireDate extends User {
         public function __construct($id, $name, $lastname, $date_of_birth, $task, $mail, $hire_date) {
-            parent::__construct($id, $name, $lastname, $date_of_birth, $task, $mail,$hire_date) ;
+            parent::__construct($id, $name, $lastname, $date_of_birth, $task, $mail) ;
             $this->hire_date = $hire_date;
         }
     }
